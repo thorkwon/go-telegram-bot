8b96e67bf9090d9ff8c4021c578988aa5c24dbc1
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2021-01-31 15:53:28 +0900

    watch/watching_clipboard: Add clipboard watching service base
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

---
+++ watch/watching_clipboard.go
@@ -0,0 +1,87 @@
+package watch
+
+import (
+	"io/ioutil"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/thorkwon/go-telegram-bot/utils"
+)
+
+var log = utils.GetLogger("watch")
+
+func init() {
+	// utils.EnableDebugLog("watch")
+}
+
+type ClipboardWatcher struct {
+	donePolling chan bool
+	watcher     struct{}
+	cb          func(string, interface{})
+	arg         interface{}
+}
+
+func ClipboardPolling(cb func(string, interface{}), arg interface{}) *ClipboardWatcher {
+	obj := &ClipboardWatcher{donePolling: make(chan bool)}
+
+	filePath, err := utils.GetConfigData("watch_file")
+	if err != nil {
+		log.Error(err)
+		return nil
+	}
+
+	obj.setCbFunc(cb, arg)
+
+	go obj.pollingProcess(filePath)
+
+	return obj
+}
+
+func (c *ClipboardWatcher) StopPolling() {
+	c.donePolling <- true
+	log.Info("Stop clipboard wathing service")
+}
+
+func (c *ClipboardWatcher) setCbFunc(cb func(string, interface{}), arg interface{}) {
+	c.cb = cb
+	c.arg = arg
+}
+
+func (c *ClipboardWatcher) pollingProcess(pollingPath string) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Panic(err)
+	}
+	defer watcher.Close()
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Debug("Watcher event : ", event)
+					data, err := ioutil.ReadFile(event.Name)
+					if err == nil {
+						c.cb(string(data)[:len(data)-1], c.arg)
+					}
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					log.Error(err)
+					return
+				}
+			}
+		}
+	}()
+
+	err = watcher.Add(pollingPath)
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Info("Clipboard watching path : ", pollingPath)
+
+	<-c.donePolling
+}
