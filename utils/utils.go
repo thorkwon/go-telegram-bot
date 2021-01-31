90ff09c7c68e716709b2456182102487c15544e4
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2021-01-31 15:52:13 +0900

    utils/utils: Add get config data
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

--- utils/utils.go
+++ utils/utils.go
@@ -2,11 +2,12 @@ package utils
 
 import (
 	"errors"
+	"io/ioutil"
 	"os"
 	"path"
 )
 
-func GetConfigDir() (string, error) {
+func getConfigDir() (string, error) {
 	configDir := path.Join(os.Getenv("HOME"), ".config/go-telegram-bot")
 
 	info, err := os.Stat(configDir)
@@ -15,4 +16,20 @@ func GetConfigDir() (string, error) {
 	}
 
 	return configDir, nil
+}
+
+func GetConfigData(configFile string) (string, error) {
+	configDir, err := getConfigDir()
+	if err != nil {
+		return configDir, err
+	}
+
+	configPath, err := ioutil.ReadFile(configDir + "/" + configFile)
+	if err != nil {
+		return "", err
+	}
+
+	filePath := string(configPath)[:len(configPath)-1]
+
+	return filePath, nil
 }
