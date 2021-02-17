2d27ddeb6c70cbd8974f82175d50d8d4cec2b3ba
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2021-02-17 23:58:15 +0900

    scripts: Add scripts for register go-telegram service
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

---
+++ scripts/README.md
@@ -0,0 +1,8 @@
+## Scripts
+To use [go-telegram-service-bot.sh][sh], [go-telegram-service-bot.service][service]
+must be registed in systemd.
+
+And modify User and ExecStart property in [go-telegram-service-bot.service][service]
+
+[sh]: ./go-telegram-service-bot.sh
+[service]: ./go-telegram-service-bot.service
---
+++ scripts/go-telegram-service-bot.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+function help()
+{
+	echo "Usage: $0 command"
+	echo "Command:"
+	echo "  status		get service status"
+	echo "  restart		service restart"
+	echo "  stop			service stop"
+	echo "  start			service start"
+	echo "  log			get service log"
+	exit 1
+}
+
+if [ $# == 0 ]; then
+	help
+fi
+
+case $1 in
+	status)
+		systemctl status go-telegram-service-bot
+		;;
+	restart)
+		sudo systemctl restart go-telegram-service-bot
+		;;
+	stop)
+		sudo systemctl stop go-telegram-service-bot
+		;;
+	start)
+		sudo systemctl start go-telegram-service-bot
+		;;
+	log)
+		echo -e "Press output log: ^c\n"
+		tail -f ~/.config/go-telegram-bot/bot.log
+		;;
+	*)
+		help
+		;;
+esac
