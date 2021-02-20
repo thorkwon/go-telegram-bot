7bdc442b0ae65cb9e35b747db76983ee213862bb
Hoegeun Kwon <hoegeun.kwon@gmail.com>
2021-02-20 17:06:36 +0900

    qbit/qbit_macro: Add feature for delete completed torrent seed in qbit
    Signed-off-by: Hoegeun Kwon <hoegeun.kwon@gmail.com>

---
+++ qbit/qbit_macro.go
@@ -0,0 +1,153 @@
+package qbit
+
+import (
+	"github.com/sclevine/agouti"
+	"github.com/thorkwon/go-telegram-bot/utils"
+)
+
+var log = utils.GetLogger(utils.GetPackageName())
+
+func init() {
+	// utils.EnableDebugLog(utils.GetPackageName())
+}
+
+type QbitMacro struct {
+	driver   *agouti.WebDriver
+	page     *agouti.Page
+	url      string
+	userName string
+	password string
+}
+
+func DeleteTorrentSeed() error {
+	obj := &QbitMacro{}
+
+	if err := obj.setQbitAccount(); err != nil {
+		return err
+	}
+
+	return obj.deleteTorrentSeed()
+}
+
+func (q *QbitMacro) setQbitAccount() error {
+	var err error
+
+	q.url, err = utils.GetConfigData("qbit_url")
+	if err != nil {
+		goto ERR
+	}
+
+	q.userName, err = utils.GetConfigData("qbit_username")
+	if err != nil {
+		goto ERR
+	}
+
+	q.password, err = utils.GetConfigData("qbit_password")
+	if err != nil {
+		goto ERR
+	}
+
+	return nil
+
+ERR:
+	log.Error(err)
+	return err
+}
+
+func (q *QbitMacro) deleteTorrentSeed() error {
+	var err error
+
+	log.Debug("Call deleteTorrentSeed")
+	if err = q.getPage(); err != nil {
+		log.Error(err)
+		return err
+	}
+	defer q.putPage()
+
+	if err = q.page.All("#username").At(0).Fill(q.userName); err != nil {
+		return err
+	}
+	if err = q.page.All("#password").At(0).Fill(q.password); err != nil {
+		return err
+	}
+
+	if err = q.page.All("#login").At(0).Click(); err != nil {
+		return err
+	}
+	// Todo. Finding the wait function waiting for the page to load
+	//       But the screenshot confirming that it was loaded
+	//       after "page.All(".torrentsTableContextMenuTarget")"
+	//       time.Sleep(time.Millisecond * 500)
+
+	if err = q.page.All("#completed_filter").At(0).Click(); err != nil {
+		return err
+	}
+
+	data := q.page.All(".torrentsTableContextMenuTarget")
+	completedSeedNum, err := data.Count()
+	if err != nil {
+		return err
+	}
+
+	log.Debug("Number of seeds completed : ", completedSeedNum)
+	if completedSeedNum != 0 {
+		if err = data.At(0).Click(); err != nil {
+			return err
+		}
+		if err = q.page.All("#deleteButton").At(0).Click(); err != nil {
+			return err
+		}
+
+		iframe := q.page.All("#confirmDeletionPage_iframe")
+		if err = iframe.At(0).SwitchToFrame(); err != nil {
+			return err
+		}
+
+		if err = q.page.All("#confirmBtn").At(0).Click(); err != nil {
+			return err
+		}
+		log.Debug("Done, Delete completed seed")
+	}
+
+	return nil
+}
+
+func (q *QbitMacro) getPage() error {
+	log.Debug("Call getPage")
+
+	q.driver = agouti.ChromeDriver(
+		agouti.ChromeOptions("args", []string{"--headless", "--disable-gpu", "--no-sandbox"}),
+	)
+	if err := q.driver.Start(); err != nil {
+		log.Error(err)
+		return err
+	}
+
+	page, err := q.driver.NewPage()
+	if err != nil {
+		log.Error(err)
+		q.driver.Stop()
+		return err
+	}
+
+	if err := page.Navigate(q.url); err != nil {
+		log.Error(err)
+		q.driver.Stop()
+		return err
+	}
+
+	q.page = page
+
+	return nil
+}
+
+func (q *QbitMacro) putPage() {
+	log.Debug("Call putPage")
+
+	if err := q.page.CloseWindow(); err != nil {
+		log.Error(err)
+	}
+	if err := q.driver.Stop(); err != nil {
+		log.Error(err)
+	}
+}
